aa1c788d134a932263b93edd6d465c79bd5fd3af
Mark Strendin <mark.strendin@lskysd.ca>
2023-09-21 08:03:46 -0600

    More progress but I'm not happy with how it works. I want it to be more linear like a script, so I might redo this using different technologies.

---
+++ src/SDS-ER.Web/SDSErrorRecord.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+public class SDSErrorRecord
+{
+    public string DivisionID { get; set; }
+    public string SchoolID { get; set; }
+    public string SchoolName { get; set; }
+    public string IndividualID { get; set; }
+    public string RecordID { get; set; }
+    public DateTime Time { get; set; }
+    public string MessageID { get; set; }
+    public string ObjectType { get; set; }
+    public string Message { get; set; }
+
+    public override string ToString()
+    {
+        StringBuilder returnMe = new StringBuilder();
+
+        returnMe.Append("{ ");
+        returnMe.Append(" DivisionID: " + this.DivisionID);
+        returnMe.Append(", SchoolID: " + this.SchoolID);
+        returnMe.Append(", SchoolName: " + this.SchoolName);
+        returnMe.Append(", IndividualID: " + this.IndividualID);
+        returnMe.Append(", RecordID: " + this.RecordID);
+        returnMe.Append(", Time: " + this.Time);
+        returnMe.Append(", MessageID: " + this.MessageID);
+        returnMe.Append(", ObjectType: " + this.ObjectType);
+        returnMe.Append(", Message: " + this.Message);
+        returnMe.Append(" }");
+        
+        return returnMe.ToString();
+    }
+}
---
+++ src/SDS-ER.Web/School.cs
@@ -0,0 +1,13 @@
+﻿public class School
+{
+    public string DAN { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public School(string dan, string name, string email)
+    {
+        this.DAN = dan;
+        this.Name = name;
+        this.Email = email;
+    }
+}
